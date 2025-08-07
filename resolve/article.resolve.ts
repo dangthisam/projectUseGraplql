1174a7ef61a71f66e5ff2ee40a2aa04faa1dfd2d
Nguyễn Văn Sâm <vansam09082004@gmail.com>
2025-08-07 22:46:34 +0700

    pagination

--- resolve/article.resolve.ts
+++ resolve/article.resolve.ts
@@ -1,104 +1,107 @@
-   
-
 import { constants } from "buffer";
 import Article from "../models/articles.model";
 import Category from "../models/category.model";
-   const resolversArticle = {
-        Query: {
-            articles: async (__, args) => {
-                try {
-                   //sort
-                    const { sortKey, sortValue } = args;
-                 const   sort ={}
-                    if (sortKey && sortValue) {
-                        sort[sortKey] = sortValue 
-                    }
-                    // Lấy danh sách bài viết từ cơ sở dữ liệu
-                    return await Article.find({ deleted: false }).sort(sort);
-                } catch (error) {
-                    console.error('Error fetching articles:', error);
-                    throw new Error('Failed to fetch articles');
-                }
-            },
-
-
-            // Hàm này sẽ lấy một bài viết theo ID
-            getArticle: async (__, args) => {
-              const { id } = args;
-                try {
-                    return await Article.findById({
-                        _id: id,
-                        deleted: false
-                    });
-                } catch (error) {
-                    console.error('Error fetching article:', error);
-                    throw new Error('Failed to fetch article');
-                }
-            },
-
-            
-        },
-      Article:{
-            category: async (article) => {
-                try {
-                    // Lấy danh mục của bài viết
-                    return await Category.findById(article.categoryId);
-                } catch (error) {
-                    console.error('Error fetching article category:', error);
-                    throw new Error('Failed to fetch article category');
-                }
-            }
-        },
-        Mutation: {
-            createArticle: async (__, args) => {
-                try {
-                    const { article } = args;
-                    const newArticle = new Article(
-                        article
-                    );
-                    return await newArticle.save();
-                } catch (error) {
-                    console.error('Error creating article:', error);
-                    throw new Error('Failed to create article');
-                }
-            },
-
-            deleteArticle: async (__, args) => {
-                const { id } = args;
-                try {
-                    const article = await Article.findByIdAndUpdate(id, { deleted: true }, { new: true });
-                    if (!article) throw new Error('Article not found');
-                    return 'Article deleted successfully';
-                } catch (error) {
-                    console.error('Error deleting article:', error);
-                    throw new Error('Failed to delete article');
-                }
-            },
-            updateArticle: async (__, args) => {
-                const { id, article } = args;
-                try {
-                   const updatedArticle = await Article.updateOne({
-                        _id: id,
-                        deleted: false
-                    }, {
-                        $set: {
-                            title: article.title,
-                            avatar: article.avatar,
-                            description: article.description,
-                            updatedAt: new Date().toISOString()
-                        }
-                    });
-
-                    if (!updatedArticle) throw new Error('Article not found or update failed');
-                    return await Article.findById(id); // Trả về bài viết đã cập nhật
-                } catch (error) {
-                    console.error('Error updating article:', error);
-                    throw new Error('Failed to update article');
-                }
-            },
-
-            
+const resolversArticle = {
+  Query: {
+    articles: async (__, args) => {
+      try {
+        //sort
+        const { sortKey, sortValue , currentPage, limit } = args;
+        const sort = {};
+        if (sortKey && sortValue) {
+          sort[sortKey] = sortValue;
         }
-    };
 
-    export default resolversArticle;
+        // Pagination
+        const skip = currentPage && limit ? (currentPage - 1) * limit : 0;
+
+        // Lấy danh sách bài viết từ cơ sở dữ liệu
+        return await Article.find({ deleted: false }).sort(sort).limit(limit).skip(skip);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+        throw new Error("Failed to fetch articles");
+      }
+    },
+
+    // Hàm này sẽ lấy một bài viết theo ID
+    getArticle: async (__, args) => {
+      const { id } = args;
+      try {
+        return await Article.findById({
+          _id: id,
+          deleted: false,
+        });
+      } catch (error) {
+        console.error("Error fetching article:", error);
+        throw new Error("Failed to fetch article");
+      }
+    },
+  },
+  Article: {
+    category: async (article) => {
+      try {
+        // Lấy danh mục của bài viết
+        return await Category.findById(article.categoryId);
+      } catch (error) {
+        console.error("Error fetching article category:", error);
+        throw new Error("Failed to fetch article category");
+      }
+    },
+  },
+  Mutation: {
+    createArticle: async (__, args) => {
+      try {
+        const { article } = args;
+        const newArticle = new Article(article);
+        return await newArticle.save();
+      } catch (error) {
+        console.error("Error creating article:", error);
+        throw new Error("Failed to create article");
+      }
+    },
+
+    deleteArticle: async (__, args) => {
+      const { id } = args;
+      try {
+        const article = await Article.findByIdAndUpdate(
+          id,
+          { deleted: true },
+          { new: true }
+        );
+        if (!article) throw new Error("Article not found");
+        return "Article deleted successfully";
+      } catch (error) {
+        console.error("Error deleting article:", error);
+        throw new Error("Failed to delete article");
+      }
+    },
+    updateArticle: async (__, args) => {
+      const { id, article } = args;
+      try {
+        const updatedArticle = await Article.updateOne(
+          {
+            _id: id,
+            deleted: false,
+          },
+          {
+            $set: {
+              title: article.title,
+              avatar: article.avatar,
+              description: article.description,
+              updatedAt: new Date().toISOString(),
+            },
+          }
+        );
+
+        if (!updatedArticle)
+          throw new Error("Article not found or update failed");
+        return await Article.findById(id); // Trả về bài viết đã cập nhật
+      } catch (error) {
+        console.error("Error updating article:", error);
+        throw new Error("Failed to update article");
+      }
+    },
+  },
+};
+
+export default resolversArticle;
--- typeDefs/typeDefsArticls.typeDef.ts
+++ typeDefs/typeDefsArticls.typeDef.ts
@@ -1,42 +1,39 @@
-
-
 // định nghĩa typedefs cho GraphQL schema tức là các kiểu dữ liệu và truy vấn mà client có thể gửi đến server
-import {gql} from "graphql-tag";
-    const typeDefsArticle = gql`
-      type Article {
-        id: ID,
-        title: String,
-        avatar: String,
-        description: String,
-        createdAt: String,
-        updatedAt: String,
-        category: Category
-      },
-  
+import { gql } from "graphql-tag";
+const typeDefsArticle = gql`
+  type Article {
+    id: ID
+    title: String
+    avatar: String
+    description: String
+    createdAt: String
+    updatedAt: String
+    category: Category
+  }
 
-      type Query {
-        articles(sortKey: String ,sortValue: String): [Article],
-        getArticle(id: ID): Article,
-    
-      }
-# định nghĩa input type cho việc tạo bài viết mới
-      # để client có thể gửi dữ liệu bài viết mới khi gọi mutation createArticle
+  type Query {
+    articles(sortKey: String,
+     sortValue: String,
+     currentPage: Int,
+     limit: Int
+     ): [Article]
+    getArticle(id: ID): Article
+  }
+  # định nghĩa input type cho việc tạo bài viết mới
+  # để client có thể gửi dữ liệu bài viết mới khi gọi mutation createArticle
 
-      input ArticleInput {
-        title: String,
-        avatar: String,
-        description: String,
-        categoryId: String
+  input ArticleInput {
+    title: String
+    avatar: String
+    description: String
+    categoryId: String
+  }
 
-        }
+  type Mutation {
+    createArticle(article: ArticleInput): Article
+    deleteArticle(id: ID): String
+    updateArticle(id: ID, article: ArticleInput): Article
+  }
+`;
 
-
-        type Mutation {
-        createArticle(article: ArticleInput): Article,
-        deleteArticle(id: ID): String,
-        updateArticle(id: ID, article: ArticleInput): Article,
-        }
-    `;
-
-
-    export default typeDefsArticle;
+export default typeDefsArticle;
